28c97fcafa69453399766b3f5d563ab896839e60
HJ1510 <yhj221004@gmail.com>
2023-02-09 13:39:18 +0900

    spread

--- index.js
+++ index.js
@@ -1,99 +1,36 @@
-/* fetch('https://learn.codeit.kr/api/topics')
-    .then((response) => response.text())
-    .then((result) => { console.log(result) }) */
+const num = [1, 2, 3];
+const num2 = [...num];
+const numPlus = [...num, 4];
+const arr = [...num, ...numPlus];
 
-// 일단 이 코드를 주석 해제하고, 아래 코드를 주석 처리해서 어떤 JSON 데이터가 오는지 확인하세요
+console.log(num);
+console.log(...num);
+console.log(num2);
+console.log(numPlus);
+console.log(...numPlus);
+console.log(arr);
+console.log(...arr);
 
-fetch("https://learn.codeit.kr/api/topics")
-  .then((response) => response.text())
-  .then((result) => {
-    const topics = JSON.parse(result); // 여기에 코드를 작성하세요
-    const beginnerLevelTopics = topics.filter(
-      (topic) => topic.difficulty === "초급"
-    );
-    console.log(beginnerLevelTopics);
-  });
-
-fetch("https://learn.codeit.kr/api/topics")
-  .then((response) => response.text())
-  .then((result) => {
-    const topics = JSON.parse(result);
-    console.log(topics[0].keyword);
-    console.log(topics[1].keyword);
-    console.log(topics[2].keyword);
-  });
-
-fetch("https://learn.codeit.kr/api/topics")
-  .then((response) => response.json())
-  .then((topics) => {
-    console.log(topics[0].keyword);
-    console.log(topics[1].keyword);
-    console.log(topics[2].keyword);
-  });
-
-fetch("https://learn.codeit.kr/api/topics")
-  .then((response) => response.text())
-  .then((result) => {
-    const articles = JSON.parse(result); // 여기에 코드를 작성하세요
-    console.log(articles);
-  });
-
-// fetch('https://learn.codeit.kr/api/members/4')
-//   .then((response) => response.text())
-//   .then((result) => { console.log(result); })
-
-const newMember = {
-  name: "Micole",
-  email: "mc@gmail.com",
-  department: "sales",
+const introduce = (name, birth, favorite) => {
+  console.log(`저는 ${name}입니다.`);
+  console.log(`${birth}년생이고`);
+  console.log(`${favorite}을 좋아합니다`);
 };
 
-fetch("https://learn.codeit.kr/api/members", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(newMember),
-})
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(result);
-  });
+const dooly = ["둘리", 1983, "라면"];
+introduce(...dooly);
 
-const member = {
-  name: "Alice",
-  email: "alice@codeitmall.kr",
-  department: "marketing",
-};
+const users = ["둘리", "마이콜", "또치"];
+const object = { ...users };
 
-fetch("https://learn.codeit.kr/api/members/2", {
-  method: "PUT",
-  body: JSON.stringify(member),
-})
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(result);
-  });
+console.log(object);
 
-fetch("https://learn.codeit.kr/api/members/2", {
-  method: "DELETE",
-})
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(result);
-  });
 
-const formData = new FormData();
-formData.append("email", email.value);
-formData.append("password", password.value);
-formData.append("nickname", nickname.value);
-formData.append("profile", image.files[0], "me.png");
+const snacks = ['원카칩', '꿀버터칩', '헛스윙칩', '태양칩', '야채시간'];
+const drinks = ['사이다', '콜라', '우유', '물', '커피', '레몬에이드'];
 
-fetch("https://learn.codeit.kr/api/members", {
-  method: "POST",
-  body: formData,
-})
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(result);
-  });
+const mySnacks = (...snacks);
+const myDrinks = [...drinks];
+
+console.log(mySnacks);
+console.log(myDrinks);
